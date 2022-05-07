0b17c1683054caee8f5796dfc2f30012a79a19ca
Ibrohim <93027235+IbrohimFayzullayev@users.noreply.github.com>
2022-05-07 12:02:46 +0500

    project finished

---
+++ src/components/Header.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <div class="ui menu">
+      <div class="header item">Our Company</div>
+      <a class="item">Add Song</a>
+      <a class="item">Jobs</a>
+      <a class="item">Locations</a>
+    </div>
+  );
+};
+
+export default Header;
