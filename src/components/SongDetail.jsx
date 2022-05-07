04718dd7e80b5b38a13f9c3b3881de75aa14d415
Ibrohim <93027235+IbrohimFayzullayev@users.noreply.github.com>
2022-05-07 12:02:51 +0500

    project finished

--- src/components/SongDetail.jsx
+++ src/components/SongDetail.jsx
@@ -1,9 +1,39 @@
 import React from "react";
+import { connect } from "react-redux";
 
 class SongDetail extends React.Component {
+  renderDetail() {
+    if (this.props.selectSong.name) {
+      return (
+        <div class="ui card">
+          <div class="image">
+            <img
+              alt="music img"
+              src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR-Apu1JPEhQk5HgxHA4jqlmFMVPVfl6ji1RQ&usqp=CAU"
+            />
+          </div>
+          <div class="content">
+            <a class="header">{this.props.selectSong.name}</a>
+            <div class="meta">
+              <span class="date">{this.props.selectSong.timer}</span>
+            </div>
+            <div class="description">{this.props.selectSong.author}</div>
+          </div>
+          <div class="extra content">
+            <a>
+              <i class="user icon"></i>
+              22 download
+            </a>
+          </div>
+        </div>
+      );
+    } else {
+      return <h2>Loading .... </h2>;
+    }
+  }
   render() {
-    return <div>Song Details</div>;
+    return <div>{this.renderDetail()}</div>;
   }
 }
-
-export default SongDetail;
+const getMyState = (state) => state;
+export default connect(getMyState)(SongDetail);
