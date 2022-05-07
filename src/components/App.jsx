c23e6951d832e9143d18b581a67d89fdbc7c9447
Ibrohim <93027235+IbrohimFayzullayev@users.noreply.github.com>
2022-05-07 12:02:42 +0500

    project finished

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,8 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
+import SongList from "./SongList";
+import SongDetail from "./SongDetail";
+import Header from "./Header";
+import AddSong from "./AddSong";
 
 const App = (props) => {
-  console.log(props);
-  return <div>App</div>;
+  const [show, setShow] = useState(false);
+
+  return (
+    <div className="ui container" style={{ marginTop: "20px" }}>
+      <Header />
+      <button
+        style={{ marginTop: "20px" }}
+        className="ui green button basic"
+        onClick={() => setShow(!show)}
+      >
+        Add Song
+      </button>
+      <div>{show ? <AddSong /> : ""}</div>
+      <div className="ui grid " style={{ marginTop: "20px" }}>
+        <div className="eight wide column">
+          <SongList />
+        </div>
+        <div className="eight wide column">
+          <SongDetail />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default App;
