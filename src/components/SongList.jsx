cf22b9b19e810f91a9e528f30045aed6c2b7c536
Ibrohim <93027235+IbrohimFayzullayev@users.noreply.github.com>
2022-05-07 12:02:56 +0500

    project finished

--- src/components/SongList.jsx
+++ src/components/SongList.jsx
@@ -1,12 +1,50 @@
 import React from "react";
-import reactRedux from "react-redux";
+import { connect } from "react-redux";
+import { selectSongAction } from "../actions";
 
-const SongList = () => {
-  return <div>Song List</div>;
+const SongList = (props) => {
+  console.log(props);
+
+  const renderList = () => {
+    return props.songList.map((val) => {
+      return (
+        <div key={val.name} className="card">
+          <div className="content">
+            <img
+              className="right floated mini ui image"
+              src="https://st.depositphotos.com/1924257/3652/i/450/depositphotos_36528877-stock-photo-headphones-with-love-music-symbol.jpg"
+            />
+            <div className="header">{val.name}</div>
+            <div className="meta">{val.timer}</div>
+            <div className="description">{val.author}</div>
+          </div>
+          <div className="extra content">
+            <div className="ui two buttons">
+              <div
+                onClick={() => {
+                  props.selectSongAction(val);
+                }}
+                className="ui basic green button"
+              >
+                Select Song
+              </div>
+            </div>
+          </div>
+        </div>
+      );
+    });
+  };
+  return (
+    <div>
+      <div className="ui cards">{renderList()}</div>
+    </div>
+  );
 };
 
 const getMyState = (data) => {
   return data;
 };
 
-export default reactRedux.connect()(SongList);
+export default connect(getMyState, {
+  selectSongAction,
+})(SongList);
