774c24e2d20b917414324f6f67367e054b3d658f
Ibrohim <93027235+IbrohimFayzullayev@users.noreply.github.com>
2022-05-07 12:03:00 +0500

    project finished

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,26 +1,28 @@
 import { combineReducers } from "redux";
-
-const songList = [
-  { name: "Uzbek", timer: "3:44", author: "Uzim" },
-  { name: "Russia", timer: "2:44", author: "Sergey" },
-  { name: "Enlish", timer: "1:23", author: "John" },
-  { name: "Turkmen", timer: "4:54", author: "Abdulla" },
-];
-
-const songListReducer = (history = songList, song) => {
-  return history;
-};
-
-const selectSongReducer = (history = [], selectSong) => {
-  if (selectSong.type === "SELECTED") {
-    return selectSong.payload;
-  }
-  return history;
-};
+import selectSongReducer from "./selectSongReducer";
+import songListReducer from "./songListReducer";
 
 const allReducers = combineReducers({
   songList: songListReducer,
-  selectedSong: selectSongReducer,
+  selectSong: selectSongReducer,
 });
 
 export default allReducers;
+
+// const songList = [
+//   { name: "Uzbek", timer: "3:44", author: "Uzim" },
+//   { name: "Russia", timer: "2:44", author: "Sergey" },
+//   { name: "Enlish", timer: "1:23", author: "John" },
+//   { name: "Turkmen", timer: "4:54", author: "Abdulla" },
+// ];
+
+// const songListReducer = (history = songList, song) => {
+//   return history;
+// };
+
+// const selectSongReducer = (history = [], selectSong) => {
+//   if (selectSong.type === "SELECTED") {
+//     return selectSong.payload;
+//   }
+//   return history;
+// };
